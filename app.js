fc9270a1437cdad4316cb40b126854d975bc25e9
yagbo <JK8574811@gmail.com>
2024-06-27 12:01:57 -0400

    userDB connected
    now when you fill out the interest form it will save your information to test/users in mongoDB

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const mainRouter = require('./routes/mainRoute');
-const interestRouter = require('./routes/userRoute');
+const userRouter = require('./routes/userRoute');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
+
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs');
@@ -13,12 +19,48 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Connect to database
+const dbURI = 'mongodb+srv://pimoshpublishing:pimosh299!@cluster0.f8ctnu9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(process.env.PORT || 3001, () => {
+            console.log('Server is running on port', process.env.PORT || 3001);
+        });
+    })
+    .catch(err => console.error(err));
+
+
+// session middleware
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: "ajfeirf90aeu9eroejfoefj",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: dbURI }),
+    cookie: { maxAge: 60 * 60 * 1000 }
+}));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.user = req.session.user || null;
+    res.locals.errorMessages = req.flash('error');
+    res.locals.successMessages = req.flash('success');
+    next();
+});
+
+
+
 // Routes
 app.use('/', mainRouter);
-app.use('/users', interestRouter);
+app.use('/users', userRouter);
 
 // Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const controller = require('../controllers/userController');
+const {isGuest, isLoggedIn} = require('../middlewares/auth');
+const { validateSignUp, validateLogIn, validateResult, validateRsvpStatus } = require('../middlewares/validator');
 const router = express.Router();
 
 
@@ -9,4 +11,10 @@ router.get('/signup', controller.getSignup);
 // Set up Interest Route 
 router.get('/login', controller.getLogin);
 
+//POST /users: create a new user account
+router.post('/', isGuest, validateSignUp, controller.create);
+
 module.exports = router;
+
+// router.post('/', isGuest, validateSignUp, validateResult, controller.create);
+// validateResult is the issue present
